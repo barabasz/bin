e35f54dc6102fdb6598d900a0f0ee2b1942d2bd8
Andrzej Barabasz <abarabasz@gmail.com>
2025-02-11 20:42:38 +0100

    update

--- lib/prompt_continue.sh
+++ lib/prompt_continue.sh
@@ -3,11 +3,11 @@
 # Function to prompt the user for continuation
 prompt_continue() {
   while true; do
-    read -p "Do you want to continue? (Y/N): " yn
-    case $yn in
-      [Yy]* ) echo "You chose to continue."; return 0;;
-      [Nn]* ) echo "You chose not to continue."; return 1;;
-      * ) echo "Please answer Y/y or N/n.";;
-    esac
+      read "yn?Do you want to continue? (Y/N): "
+      case $yn in
+          [Yy]* ) echo "You chose to continue."; return 0;;
+          [Nn]* ) echo "You chose not to continue."; return 1;;
+          * ) echo "Please answer Y/y or N/n.";;
+      esac
   done
 }
