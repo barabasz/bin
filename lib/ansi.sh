dd1e77d896d273279ceb19ed20274b94467f614e
Andrzej Barabasz <abarabasz@gmail.com>
2025-02-23 00:59:32 +0100

    fixes

--- lib/ansi.sh
+++ lib/ansi.sh
@@ -2,41 +2,71 @@
 #
 # ANSI escape code warper
 #
-# Usage: 
-#   ansi <style> <background> <foreground>
-#   ansi reset [option]
-# Style:
-#   style:                    ansi <style>
-# Background:
-#   color:                    ansi bg <color>
-#   8-bit color:              ansi bg 8bit {0..255}
-#   rgb color:                ansi bg rgb {0..255} {0..255} {0..255}
-#   default color:            ansi bg default
-# Foreground:
-#   color:                    ansi <color>
-#   bright color:             ansi bright <color>
-#   8-bit color:              ansi 8bit {0..255}
-#   rgb color:                ansi rgb {0..255} {0..255} {0..255}
-#   default color:            ansi default
-# Reset:
-#   reset style:              ansi reset <style>
-#   reset all:                ansi reset
-# Colors:
-#   black      red             yellow
-#   white      green           magenta = purple
-#   default    blue            cyan
-# Styles:
-#   bold       underline        dim
-#   italic     strikethrough    blink
-#   reverse    overline         invisible
-# Examples:
-#   printf "$(ansi bold red)bold red$(ansi reset) reset"
-#   echo "$(ansi yellow)yellow $(ansi reverse)reverse$(ansi reset reverse) normal$(ansi default) default"
-#
 # SGR (Select Graphic Rendition) parameters of ANSI escape codes
 # man: https://man7.org/linux/man-pages/man4/console_codes.4.html
 
+ansi::info() {
+    \cat << EOF
+$(ansi bold yellow)ANSI escape code warper$(ansi reset)
+
+$(ansi bold)Usage$(ansi reset): 
+  $(ansi yellow)ansi$(ansi reset) help                 show help
+  $(ansi yellow)ansi$(ansi reset) info                 show this info
+  $(ansi yellow)ansi$(ansi reset) example              show examples
+  $(ansi yellow)ansi$(ansi reset) <style>              set style
+  $(ansi yellow)ansi$(ansi reset) <foreground>         set foreground color
+  $(ansi yellow)ansi$(ansi reset) bg <background>      set background color
+  $(ansi yellow)ansi$(ansi reset) reset [option]       reset style
+  $(ansi yellow)ansi$(ansi reset) show <command>       only show ANSI code
+
+EOF
+}
+
+ansi::help() {
+    ansi::info
+    \cat << EOF
+$(ansi bold)Expected arguments order$(ansi reset):
+  $(ansi yellow)ansi$(ansi reset) [style] [[bright] <foreground>] [bg <background>] [reset [style]]
+
+$(ansi bold)Style$(ansi reset):
+  style:                    ansi $(ansi bold purple)<style>$(ansi reset)
+
+$(ansi bold)Foreground$(ansi reset):
+  color:                    $(echo "\e[33mansi\e[39m") $(ansi bold cyan)<color>$(ansi reset)
+  bright color:             $(echo "\e[33mansi\e[39m") bright $(ansi bold cyan)<color>$(ansi reset)
+  8-bit color:              $(echo "\e[33mansi\e[39m") 8bit {0..255}
+  rgb color:                $(echo "\e[33mansi\e[39m") rgb {0..255} {0..255} {0..255}
+  default color:            $(echo "\e[33mansi\e[39m") default
+
+$(ansi bold)Background$(ansi reset):
+  color:                    $(echo "\e[33mansi\e[39m") bg $(ansi bold cyan)<color>$(ansi reset)
+  8-bit color:              $(echo "\e[33mansi\e[39m") bg 8bit {0..255}
+  rgb color:                $(echo "\e[33mansi\e[39m") bg rgb {0..255} {0..255} {0..255}
+  default color:            $(echo "\e[33mansi\e[39m") bg default
+
+$(ansi bold)Reset$(ansi reset):
+  reset style:              $(echo "\e[33mansi\e[39m") reset $(ansi bold purple)<style>$(ansi reset)
+  reset all:                $(echo "\e[33mansi\e[39m") reset
+
+$(ansi bold cyan)Colors$(ansi reset):
+  black     red     yellow
+  white     green   magenta = purple
+  default   blue    cyan
+
+$(ansi bold purple)Styles$(ansi reset):
+  bold      underline      dim
+  italic    strikethrough  blink
+  reverse   overline       invisible
+
+$(ansi bold)Examples$(ansi reset):
+  printf "\$(ansi bold red)bold red\$(ansi reset) reset"
+  echo "\$(ansi yellow)yellow \$(ansi reverse)reverse\$(ansi reset reverse) normal\$(ansi default) default"
+
+EOF
+}
+
 ansi::example() {
+    ansi::info
     echo "Styles:"
     echo "  • $(ansi bold)bold$(ansi reset bold)"
     echo "  • $(ansi italic)italic$(ansi reset italic)"
@@ -47,105 +77,112 @@ ansi::example() {
     echo "  • $(ansi dim)dim$(ansi reset dim)"
     echo "  • $(ansi blink)blink$(ansi reset blink)"
     echo "  • $(ansi invisible)red$(ansi reset invisible) (invisible)"
+    echo "  • $(ansi default)default$(ansi reset)"
     echo "Foreground:"
     echo "  • $(ansi red)red$(ansi default)"
     echo "  • $(ansi bright red)bright red $(ansi default)"
     echo "  • $(ansi 8bit 196)8bit 196$(ansi default)"
     echo "  • $(ansi rgb 255 0 0)rgb 255 0 0$(ansi default)"
     echo "Background:"
-    echo "  • $(ansi bg red)bg red$(ansi bg default)"
+    echo "  • $(ansi bg green)bg green$(ansi bg default)"
     echo "  • $(ansi bg 8bit 196)bg 8bit 196$(ansi bg default)"
-    echo "  • $(ansi bg rgb 255 0 0)bg rgb 255 0 0$(ansi bg default)"
+    echo "  • $(ansi bg rgb 0 0 255)bg rgb 0 0 255$(ansi bg default)"
     echo "Compound expression:"
-    echo "  • $(ansi bg rgb 0 255 0 red)bg rgb 0 255 red$(ansi reset)"
     echo "  • $(ansi bold yellow)bold yellow$(ansi reset)"
     echo "  • $(ansi italic cyan)italic cyan $(ansi reset)"
+    echo "  • $(ansi bg rgb 0 255 0 red)bg rgb 0 255 red$(ansi reset)"
+    echo "  • $(ansi italic yellow bg blue)italic yellow bg blue$(ansi reset)"
 }
 
 ansi::style() {
-    case "$@" in
-        regular*) mod=0 ;;
-        bold*) mod=1 ;;
-        dim*) mod=2 ;;
-        italic*) mod=3 ;;
-        underline*) mod=4 ;;
-        dunderline*) mod=21 ;;
-        blink*) mod=5 ;;
-        fastblink*) mod=6 ;;
-        reverse*) mod=7 ;;
-        invisible*) mod=8 ;;
-        strikethrough*) mod=9 ;;
-        overline*) mod=53 ;;
+    case "$1" in
+        regular) mod=0 ;;
+        bold) mod=1 ;;
+        dim) mod=2 ;;
+        italic) mod=3 ;;
+        underline) mod=4 ;;
+        dunderline) mod=21 ;;
+        blink) mod=5 ;;
+        fastblink) mod=6 ;;
+        reverse) mod=7 ;;
+        invisible) mod=8 ;;
+        strikethrough) mod=9 ;;
+        overline) mod=53 ;;
     esac
 }
 
 ansi::reset() {
-    mod=0
-    case "$@" in
-        bold*) mod=22 ;;
-        dim*) mod=22 ;;
-        italic*) mod=23 ;;
-        underline*) mod=24 ;;
-        blink*) mod=25 ;;
-        fastblink*) mod=26 ;;
-        reverse*) mod=27 ;;
-        invisible*) mod=28 ;;
-        strikethrough*) mod=29 ;;
-        overline*) mod=55 ;;
+    case "$1" in
+        all) mod=0 ;;
+        bold) mod=22 ;;
+        dim) mod=22 ;;
+        italic) mod=23 ;;
+        underline) mod=24 ;;
+        blink) mod=25 ;;
+        fastblink) mod=26 ;;
+        reverse) mod=27 ;;
+        invisible) mod=28 ;;
+        strikethrough) mod=29 ;;
+        overline) mod=55 ;;
+        *) echo "Invalid reset style: $1"; return 1 ;;
     esac
 }
 
 # foreground colors {30..37}
 ansi::foreground() {
     shift=1
-    case "$@" in
-        black*) color=30 ;;
-        red*) color=31 ;;
-        green*) color=32 ;;
-        yellow*) color=33 ;;
-        blue*) color=34 ;;
-        magenta*) color=35 ;;
-        purple*) color=35 ;;
-        cyan*) color=36 ;;
-        white*) color=37 ;;
-        default*) color=39 ;;
-        gray*) color=90 ;;
-        rgb*) color="38;2;$2;$3;$4"; shift=4 ;;
-        8bit*) color="38;5;$2"; shift=2 ;;
+    case "$1" in
+        black) color=30 ;;
+        red) color=31 ;;
+        green) color=32 ;;
+        yellow) color=33 ;;
+        blue) color=34 ;;
+        magenta) color=35 ;;
+        purple) color=35 ;;
+        cyan) color=36 ;;
+        white) color=37 ;;
+        rgb) color="38;2;$2;$3;$4"; shift=4 ;;
+        8bit) color="38;5;$2"; shift=2 ;;
+        default) color=39 ;;
+        gray) color=90 ;;
+        *) echo "Invalid style or foreground color: $1"; return 1 ;;
     esac
+    shift $shift
 }
 
 # background colors {40..47}
 ansi::background() {
     shift=1
-    case "$@" in
-        black*) bcolor=40 ;;
-        red*) bcolor=41 ;;
-        green*) bcolor=42 ;;
-        yellow*) bcolor=43 ;;
-        blue*) bcolor=44 ;;
-        magenta*) bcolor=45 ;;
-        purple*) bcolor=45 ;;
-        cyan*) bcolor=46 ;;
-        white*) bcolor=47 ;;
-        default*) bcolor=49 ;;
-        rgb*) bcolor="48;2;$2;$3;$4"; shift=4 ;;
-        8bit*) bcolor="48;5;$2"; shift=2 ;;
+    case "$1" in
+        black) bcolor=40 ;;
+        red) bcolor=41 ;;
+        green) bcolor=42 ;;
+        yellow) bcolor=43 ;;
+        blue) bcolor=44 ;;
+        magenta) bcolor=45 ;;
+        purple) bcolor=45 ;;
+        cyan) bcolor=46 ;;
+        white) bcolor=47 ;;
+        default) bcolor=49 ;;
+        rgb) bcolor="48;2;$2;$3;$4"; shift=4 ;;
+        8bit) bcolor="48;5;$2"; shift=2 ;;
+        *) echo "Invalid background color: $1"; return 1 ;;
     esac
 }
 
 # foreground bright colors {90..97}
 ansi::bright() {
-    case "$@" in
-        black*) color=90 ;;
-        red*) color=91 ;;
-        green*) color=92 ;;
-        yellow*) color=93 ;;
-        blue*) color=94 ;;
-        magenta*) color=95 ;;
-        purple*) color=95 ;;
-        cyan*) color=96 ;;
-        white*) color=97 ;;
+    case "$1" in
+        black) color=90 ;;
+        red) color=91 ;;
+        green) color=92 ;;
+        yellow) color=93 ;;
+        blue) color=94 ;;
+        magenta) color=95 ;;
+        purple) color=95 ;;
+        cyan) color=96 ;;
+        white) color=97 ;;
+        *) echo "Invalid bright color name: $1"; return 1 ;;
     esac
 }
 
@@ -157,39 +194,96 @@ ansi::code() {
     s2=${color:+${bcolor:+;}}
     color=${color:+$color}
     sufix="m"
-    echo "${prefix}${mod}$s1${bcolor}$s2${color}$sufix"
+    if [[ $show -eq 1 ]]; then
+        echo -n "\\"
+        echo "${prefix:1}${mod}$s1${bcolor}$s2${color}$sufix"
+    else
+        echo -n "${prefix}${mod}$s1${bcolor}$s2${color}$sufix"
+    fi
 }
 
 ansi::make() {
-
-    # styles
+    # reset
     if [[ "$*" == "reset"* ]]; then
         shift
-        ansi::reset $@
-        if (( $mod > 0 )); then shift; fi
-    else
+        if (( $# == 0 )); then
+            ansi::reset all
+        else
+            ansi::reset $1
+            shift
+        fi
+    fi
+    # set style or color
+    if (( $# > 0 )); then
+        # style
         ansi::style $@
-        if [[ -v mod ]]; then shift; fi
+        if [[ -n $mod ]]; then
+            shift
+        fi
+
+        # background
+        if [[ "$*" == "bg"* ]]; then
+            if [[ $# -eq 1 ]]; then
+                echo "Missing background color name"
+                return 1
+            fi
+            shift
+            ansi::background $@ || return 1
+            shift $shift
+            unset shift
+        fi 
+
+        # foreground
+        if [[ "$*" == "bright"* ]]; then
+            if [[ $# -eq 1 ]]; then
+                echo "Missing bright color name"
+                return 1
+            fi
+            shift
+            ansi::bright $@ || return 1
+            shift
+        else
+            if [[ $# -gt 0 ]]; then
+                ansi::foreground $@ || return 1
+                shift $shift
+                unset shift
+            fi
+        fi
+ 
+        # background again (when set with foreground)
+        if [[ "$*" == "bg"* ]]; then
+            if [[ $# -eq 1 ]]; then
+                echo "Missing background color name"
+                return 1
+            fi
+            shift
+            ansi::background $@
+            shift $shift
+            unset shift
+        fi 
+
+        if (( $# > 0 )); then
+            echo "Too many arguments: $@"
+            return 1
+        fi
     fi
-    # backgrouund
-    if [[ "$*" == "bg"* ]]; then
-        shift
-        ansi::background $@
-        shift $#
-    fi
-    # foreground
-    if [[ "$*" == "bright"* ]]; then
-        shift
-        ansi::bright $@
-        shift
-    else
-        ansi::foreground $@
-        shift $shift
-    fi
-    ansi::code
 }
 
 ansi() {
-    ansi::make "$@ tail tail tail"
+    unset mod color bcolor show shift
+    if [[ $# == 0 ]]; then
+        ansi::info
+        return 0
+    elif [[ "$1" == "help" ]]; then
+        ansi::help
+        return 0
+    elif [[ "$1" == "example" ]]; then
+        ansi::example
+        return 0
+    elif [[ "$1" == "show" ]]; then
+        show=1
+        shift
+    fi
+    ansi::make "$@" || return 1
     ansi::code
-}
+}   
